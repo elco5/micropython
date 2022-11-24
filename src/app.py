d9d9a41ac676a0c55786662cb05c1df5927b70c7
Eli Countrywood <countrywoodster@gmail.com>
2022-11-24 12:28:53 -0800

    add pulse driver

--- src/app.py
+++ src/app.py
@@ -5,22 +5,26 @@ from time import sleep_ms
 from time import ticks_ms
 from time import ticks_diff
 
-from lib import bucket
+from lib import tach
 
 LED_PIN = 32
 SWITCH_PIN = 25
 SLEEP_TIME_MS = 300
 
-
+real_speed_low = True
 led = Pin(LED_PIN, Pin.OUT)
 switch = Pin(SWITCH_PIN, Pin.IN, Pin.PULL_UP)
-bucket = bucket.Bucket(3)
+tach = tach.Tach(3)
+
+verbose = True
 
 
 def handle_interrupt(pin):
     global switch_closed
-    switch_closed = True
+    switch_closed = True 
+    # do things each switch closure
 
+# configure interrupt on switch pin
 switch.irq(trigger=Pin.IRQ_FALLING,
            handler=handle_interrupt)
 
@@ -29,24 +33,33 @@ prev_pulse = ticks_ms()
 
 switch_closed = False
 while True:
-
+    
     if switch_closed:
+        # loop_time_begin = ticks_ms()
+        # time critical tasks... 
         led.value(1)
-
         this_pulse = ticks_ms()
         period = ticks_diff(this_pulse, prev_pulse)
-        bucket.push(period)
         prev_pulse = ticks_ms()
+        # end time critcal tasks...
 
-        print(f'Pulse Detected! - Time: \
-            this: {this_pulse},\
-            prev: {prev_pulse},\
-            period = {period} ms')
-        
-        bucket.show()
-
+        print('Pulse Detected!')
+        tach.record(period)
         sleep_ms(SLEEP_TIME_MS)
         led.value(0)
+        
+
+
+        if verbose: tach.show()
         print('Waiting...')
 
         switch_closed = False
+        # loop_time_end = ticks_ms()
+        # print('loop_time', ticks_diff(loop_time_end, loop_time_begin))
+
+    if not tach.hi_speed:
+        # pulser.pass_pulse()
+        pass # pass the pulse
+    else:
+        # high speed state == True
+        pass # 16 mph pulse 
