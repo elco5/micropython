83be3b3912be52ca097ccacf7ef1f39e604a7b12
Eli Countrywood <countrywoodster@gmail.com>
2022-12-01 22:20:13 -0800

    velo calc

--- etc/velo_calc.py
+++ etc/velo_calc.py
@@ -6,13 +6,13 @@ pi = np.pi
 def mph_2_T(mph):
     d = 0.7
     v_ms = mph/2.23694
-    return (pi*d)/v_ms
+    return np.round((pi*d)/v_ms,3)
 
 
 def T_2_mph(T):
     d = 0.7
     v = (pi*d)/T
-    return v*2.23694
+    return np.round(v*2.23694,3)
     
 
 ''' conclusion: sample switch and 
@@ -21,12 +21,10 @@ accept intterupts in at least
 '''
 
 print(f'bike is restricted to 19 mph. interval: {mph_2_T(19)}')
-print(f'hack the sensor so bike thinks it is going 15 mph. interval: {mph_2_T(15)}')
+# 19mph -> 259 ms
+print(f'hack the sensor so bike thinks it is going 15 mph. interval: {mph_2_T(15)}') 
+# 15mph -> 328 ms
 
 
-
-print(T_2_mph(0.12))
-
-print(mph_2_T(40))
 
 
